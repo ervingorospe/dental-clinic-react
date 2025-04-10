c5dcfdcaad764799bb7f8fa5944170cb1da74a47
Ervin Gorospe <ervingorospe.code@gmail.com>
2025-04-11 06:43:50 +0800

    debug reading env port

--- src/utils/auth.tsx
+++ src/utils/auth.tsx
@@ -1,5 +1,6 @@
 export const checkAuthStatus = async (): Promise<{ isAuthenticated: boolean, user?: any }> => {
   const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
+  console.log(apiBaseUrl);
 
   try {
     let response = await fetch(`${apiBaseUrl}/api/auth/authenticate`, {
