94d81773351c2bc41c8585a58309672dd4cd3fe4
Ervin Gorospe <ervingorospe.code@gmail.com>
2025-05-21 21:31:01 +0800

    test creating branch

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -27,7 +27,7 @@ interface AuthProviderProps {
 export const useAuth = () => {
   const context = useContext(AuthContext);
   if (!context) {
-    throw new Error('useAuth must be used within an AuthProvider');
+    throw new Error('useAuth must be used within an AuthProvider aa');
   }
   return context;
 };
